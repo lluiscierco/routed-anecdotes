47cf5fab8315933ce8e360acc3876d38bd00c57e
lluis <63962612+lluiscierco@users.noreply.github.com>
2024-02-21 12:09:59 +0000

    addes custom hook useField

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,8 @@ import {
   useNavigate,
 } from "react-router-dom";
 
+import { useField } from "./hooks/index.js";
+
 const Menu = () => {
   const padding = {
     paddingRight: 5,
@@ -86,18 +88,21 @@ const Footer = () => (
 );
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState("");
-  const [author, setAuthor] = useState("");
-  const [info, setInfo] = useState("");
+  //const [content, setContent] = useState("");
+  //const [author, setAuthor] = useState("");
+  //const [info, setInfo] = useState("");
+  const content = useField("content");
+  const author = useField("author");
+  const info = useField("url");
 
   const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      url: info.value,
       votes: 0,
     });
     navigate("/");
@@ -109,27 +114,15 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input
-            name="content"
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-          />
+          <input {...content} />
         </div>
         <div>
           author
-          <input
-            name="author"
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
-          />
+          <input {...author} />
         </div>
         <div>
           url for more info
-          <input
-            name="info"
-            value={info}
-            onChange={(e) => setInfo(e.target.value)}
-          />
+          <input {...info} />
         </div>
         <button>create</button>
       </form>
